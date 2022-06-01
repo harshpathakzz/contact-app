3b1a7a731d39117b60ce04e80a0ecf2a083d01c7
harshpathakzz <harshpathak2017@gmail.com>
2022-06-01 16:59:01 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header'
+import ContactList from './ContactList'
+import AddContacts from './AddContacts'
+import ContactCard from './ContactCard'
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
